67c8652d74bb3b7fa558d15ee33f40c4ea89e9c2
YizhiHong <yhong25@hawk.iit.edu>
2018-10-14 20:58:47 -0500

    update 460.lcu caches

---
+++ 460. LFU Cache.js
@@ -0,0 +1,352 @@
+/**
+Design and implement a data structure for Least Frequently Used (LFU) cache. It should support the following operations: get and put.
+
+get(key) - Get the value (will always be positive) of the key if the key exists in the cache, otherwise return -1.
+put(key, value) - Set or insert the value if the key is not already present. When the cache reaches its capacity, it should invalidate the least frequently used item before inserting a new item. For the purpose of this problem, when there is a tie (i.e., two or more keys that have the same frequency), the least recently used key would be evicted.
+
+Follow up:
+Could you do both operations in O(1) time complexity?
+
+Example:
+
+LFUCache cache = new LFUCache( 2 //capacity);
+
+cache.put(1, 1);
+cache.put(2, 2);
+cache.get(1);       // returns 1
+cache.put(3, 3);    // evicts key 2
+cache.get(2);       // returns -1 (not found)
+cache.get(3);       // returns 3.
+cache.put(4, 4);    // evicts key 1.
+cache.get(1);       // returns -1 (not found)
+cache.get(3);       // returns 3
+cache.get(4);       // returns 4
+*/
+
+
+/**
+ * @param {number} capacity
+ */
+class LFUCache {
+    constructor(capacity){
+        this.capacity = capacity
+        this.set = {} // key -> node
+        this.lists = {} // count -> listNode
+        this.min = 1
+    }
+    get(key){
+        if(!this.set.hasOwnProperty(key)) return -1
+        // update the counter
+        this.update(this.set[key]) 
+    
+        return this.set[key].val
+    }
+    
+    update(node){
+        // 1.update counter
+        let prev_count = node.count
+        node.count++
+        let curr_count = node.count
+
+        // 2.delete entry node in counter lists
+        this.lists[prev_count].delete(node.listNode.value)
+
+        if(this.lists[prev_count].head === null && this.lists[prev_count].tail === null && prev_count === this.min){
+            // delete the min counter 
+            delete this.lists[prev_count]
+            this.min = this.min + 1
+        }
+
+        // 3.append the node on the front
+        if(!this.lists.hasOwnProperty(curr_count)){
+            this.lists[curr_count] = new DoublyLinkedList()
+        }
+        this.lists[curr_count].prepend(node.key)
+
+        // 4.update new pointer
+        node.listNode = this.lists[curr_count].head
+    }
+    
+    put(key,value){
+        if(this.capacity <= 0) return;
+    
+        //if key already exist
+        if(this.set.hasOwnProperty(key)){
+            this.set[key].val = value
+            this.update(this.set[key])
+        }else{
+              // if size reach the capacity
+            if(Object.keys(this.set).length === this.capacity ){
+                //delete the last Listnode 
+                let evict = this.lists[this.min].tail.value
+
+                this.lists[this.min].deleteTail()
+
+                //delete 
+                delete this.set[evict]
+            }
+
+            this.min = 1
+
+            if(!this.lists.hasOwnProperty(this.min)){
+                this.lists[this.min] = new DoublyLinkedList()
+            }
+
+            this.lists[this.min].prepend(key)
+
+            this.set[key] = new node(key,value,this.min, this.lists[this.min].head)
+        }
+    
+    }
+    
+}
+/** 
+ * Node object
+ * @param {number} key
+ * @param {number} value
+ * @param {number} counter
+ * @param {listNode} point to double linked-list
+ */
+class node {
+    constructor(key, value, count, listNode){
+        this.key = key;
+        this.val = value;
+        this.count = count;
+        this.listNode = listNode;
+    }
+}
+
+/** 
+ * Your LFUCache object will be instantiated and called as such:
+ * var obj = Object.create(LFUCache).createNew(capacity)
+ * var param_1 = obj.get(key)
+ * obj.put(key,value)
+ */
+
+/** assets **/
+class Comparator {
+  /**
+   * @param {function(a: *, b: *)} [compareFunction]
+   */
+  constructor(compareFunction) {
+    this.compare = compareFunction || Comparator.defaultCompareFunction;
+  }
+
+  /**
+   * @param {(string|number)} a
+   * @param {(string|number)} b
+   * @returns {number}
+   */
+  static defaultCompareFunction(a, b) {
+    if (a === b) {
+      return 0;
+    }
+
+    return a < b ? -1 : 1;
+  }
+
+  equal(a, b) {
+    return this.compare(a, b) === 0;
+  }
+
+  lessThan(a, b) {
+    return this.compare(a, b) < 0;
+  }
+
+  greaterThan(a, b) {
+    return this.compare(a, b) > 0;
+  }
+
+  lessThanOrEqual(a, b) {
+    return this.lessThan(a, b) || this.equal(a, b);
+  }
+
+  greaterThanOrEqual(a, b) {
+    return this.greaterThan(a, b) || this.equal(a, b);
+  }
+
+  reverse() {
+    const compareOriginal = this.compare;
+    this.compare = (a, b) => compareOriginal(b, a);
+  }
+}
+
+class DoublyLinkedListNode {
+  constructor(value, next = null, previous = null) {
+    this.value = value;
+    this.next = next;
+    this.previous = previous;
+  }
+
+  toString(callback) {
+    return callback ? callback(this.value) : `${this.value}`;
+  }
+}
+
+class DoublyLinkedList {
+  /**
+   * @param {Function} [comparatorFunction]
+   */
+  constructor(comparatorFunction) {
+    /** @var DoublyLinkedListNode */
+    this.head = null;
+
+    /** @var DoublyLinkedListNode */
+    this.tail = null;
+
+    this.compare = new Comparator(comparatorFunction);
+  }
+
+  /**
+   * @param {*} value
+   * @return {DoublyLinkedList}
+   */
+  prepend(value) {
+    // Make new node to be a head.
+    const newNode = new DoublyLinkedListNode(value, this.head);
+
+    // If there is head, then it won't be head anymore.
+    // Therefore, make its previous reference to be new node (new head).
+    // Then mark the new node as head.
+    if (this.head) {
+      this.head.previous = newNode;
+    }
+    this.head = newNode;
+
+    // If there is no tail yet let's make new node a tail.
+    if (!this.tail) {
+      this.tail = newNode;
+    }
+
+    return this;
+  }
+
+  /**
+   * @param {*} value
+   * @return {DoublyLinkedList}
+   */
+  append(value) {
+    const newNode = new DoublyLinkedListNode(value);
+
+    // If there is no head yet let's make new node a head.
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+
+      return this;
+    }
+
+    // Attach new node to the end of linked list.
+    this.tail.next = newNode;
+
+    // Attach current tail to the new node's previous reference.
+    newNode.previous = this.tail;
+
+    // Set new node to be the tail of linked list.
+    this.tail = newNode;
+
+    return this;
+  }
+
+  /**
+   * @param {*} value
+   * @return {DoublyLinkedListNode}
+   */
+  delete(value) {
+    if (!this.head) {
+      return null;
+    }
+
+    let deletedNode = null;
+    let currentNode = this.head;
+
+    while (currentNode) {
+      if (this.compare.equal(currentNode.value, value)) {
+        deletedNode = currentNode;
+
+        if (deletedNode === this.head) {
+          // If HEAD is going to be deleted...
+
+          // Set head to second node, which will become new head.
+          this.head = deletedNode.next;
+
+          // Set new head's previous to null.
+          if (this.head) {
+            this.head.previous = null;
+          }
+
+          // If all the nodes in list has same value that is passed as argument
+          // then all nodes will get deleted, therefore tail needs to be updated.
+          if (deletedNode === this.tail) {
+            this.tail = null;
+          }
+        } else if (deletedNode === this.tail) {
+          // If TAIL is going to be deleted...
+
+          // Set tail to second last node, which will become new tail.
+          this.tail = deletedNode.previous;
+          this.tail.next = null;
+        } else {
+          // If MIDDLE node is going to be deleted...
+          const previousNode = deletedNode.previous;
+          const nextNode = deletedNode.next;
+
+          previousNode.next = nextNode;
+          nextNode.previous = previousNode;
+        }
+      }
+
+      currentNode = currentNode.next;
+    }
+
+    return deletedNode;
+  }
+
+  /**
+   * @return {DoublyLinkedListNode}
+   */
+  deleteTail() {
+    if (!this.tail) {
+      // No tail to delete.
+      return null;
+    }
+
+    if (this.head === this.tail) {
+      // There is only one node in linked list.
+      const deletedTail = this.tail;
+      this.head = null;
+      this.tail = null;
+
+      return deletedTail;
+    }
+
+    // If there are many nodes in linked list...
+    const deletedTail = this.tail;
+
+    this.tail = this.tail.previous;
+    this.tail.next = null;
+
+    return deletedTail;
+  }
+
+  /**
+   * @return {DoublyLinkedListNode}
+   */
+  deleteHead() {
+    if (!this.head) {
+      return null;
+    }
+
+    const deletedHead = this.head;
+
+    if (this.head.next) {
+      this.head = this.head.next;
+      this.head.previous = null;
+    } else {
+      this.head = null;
+      this.tail = null;
+    }
+
+    return deletedHead;
+  }
+}
