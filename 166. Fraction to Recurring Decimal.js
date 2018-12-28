ac17235b2eee4bbbdc608d12abe137ec97cc58ed
YizhiHong <yhong25@hawk.iit.edu>
2018-12-27 20:44:49 -0600

    update 166

--- 166. Fraction to Recurring Decimal.js
+++ 166. Fraction to Recurring Decimal.js
@@ -58,3 +58,36 @@ var fractionToDecimal = function(numerator, denominator) {
     
     return res
 };
+
+//better
+var fractionToDecimal = function(numerator, denominator) {
+  if (numerator === 0) return '0';
+  let res = '';
+
+  if (Math.sign(numerator) !== Math.sign(denominator)) res += '-';
+
+  let n = Math.abs(numerator);
+  const d = Math.abs(denominator);
+
+  res += Math.floor(n / d);
+  n %= d;
+
+  if (n === 0) return res;
+
+  res += '.';
+
+  let map = {};
+
+  while (n !== 0) {
+    map[n] = res.length;
+
+    n *= 10;
+    res += Math.floor(n / d);
+    n %= d;
+
+    const i = map[n]; // repeat starting index
+    if (i !== undefined) return `${res.substr(0, i)}(${res.substr(i)})`;
+  }
+
+  return res;
+};
