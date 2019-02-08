fef6eb126d07840f3b174c4df5a4e795b68b3a6e
YizhiHong <yhong25@hawk.iit.edu>
2019-02-07 18:35:57 -0800

    update

--- 653. Two Sum IV - Input is a BST.js
+++ 653. Two Sum IV - Input is a BST.js
@@ -29,7 +29,7 @@ Target = 28
 Output: False
 */
 
-//O(n) O(n)
+//O(n) O(n) Recusive
 var findTarget = function(root, k) {
     let map = new Set(), flag = false
     
@@ -46,3 +46,53 @@ var findTarget = function(root, k) {
     inorder(root)
     return flag ? true : false    
 };
+
+//O(n) O(n) morris
+var findTarget = function(root, k) {
+    let curr = root
+    
+    const hasComplement = (node,val,head) => {
+        let map = new Set()
+        while(head){
+            // console.log(head.val)
+            map.add(head. val)
+            if( head.val === val && node!== head) {
+                return true
+            }else if(head.val < val) {
+                head = head.right
+            }else {
+                head = head.left
+            }
+            if(head&& map.has(head.val)) return false 
+        }
+        return false
+    }
+    
+    while(curr !== null){
+        if(curr.left !== null){
+            let temp = curr.left
+            // looking for the right most node of the left subtree
+            while(temp.right !== null && temp.right !== curr){
+                temp = temp.right
+            }
+            // first visit, connect a bridge
+            if(temp.right === null){
+                temp.right = curr
+                curr = curr.left
+            }else{
+                // visit value
+                temp.right = null
+                if(hasComplement(curr, k-curr.val, root)) return true
+                curr = curr.right
+                
+            }
+        }else{
+            // visit value
+            if(hasComplement(curr, k-curr.val, root)) return true
+            curr = curr.right
+        }
+    }   
+    return false
+};
+
+
