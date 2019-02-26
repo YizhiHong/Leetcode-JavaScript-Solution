a8a0d989ee40966091837d092de47f2fb155406d
YizhiHong <yhong25@hawk.iit.edu>
2019-02-25 21:44:46 -0800

    update

--- 215. Kth Largest Element in an Array.js
+++ 215. Kth Largest Element in an Array.js
@@ -13,3 +13,12 @@
 
 
 
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findKthLargest = function(nums, k) {
+    nums.sort((a,b) => b-a)
+    return nums[k-1]
+};
