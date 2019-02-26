248fdfeab9a63f5d8bba8ebb0c080e386ee3ab0b
YizhiHong <yhong25@hawk.iit.edu>
2019-02-26 12:48:55 -0800

    update

--- 215. Kth Largest Element in an Array.js
+++ 215. Kth Largest Element in an Array.js
@@ -14,11 +14,56 @@
 
 
 /**
- * @param {number[]} nums
- * @param {number} k
- * @return {number}
+Solution 1: sort - O(nlogn) O(1)
  */
+
 var findKthLargest = function(nums, k) {
     nums.sort((a,b) => b-a)
     return nums[k-1]
+};
+
+/**
+Solution 2: Priority Queue - minQueue O(nlogk), maxQueue O(n + klogn)
+ */
+
+
+/**
+Solution 3: Quick Select -  Hoare Partition O(n) worst O(n^2)
+ */
+
+ var findKthLargest = function(nums, k) {
+    let [lo,hi] = [0,nums.length-1],
+        kth = (nums.length-k),
+        res
+    
+    // partion like quicksort
+    const _partition = (start,end) => {
+        // let pivot = ~~(Math.random() * (end-start+1) + start)
+        let pivot = start
+        const _swap = (a,b) =>{
+            [nums[a],nums[b]] = [nums[b],nums[a]]
+        }
+        
+        while (start <= end){
+            while(start <= end && nums[start] <= nums[pivot]) start++
+            while(start <= end && nums[pivot] <= nums[end]) end--
+            if(start > end){
+                break
+            }
+            _swap(start,end)
+        }
+        _swap(pivot,end)
+        return end
+    }
+
+    while(lo <= hi){
+        res = _partition(lo,hi)
+        if(res === kth) {
+            return nums[res]
+        }else if(res > kth){
+            hi = res - 1
+        }else{
+            lo = res + 1
+        }
+    }
 };
