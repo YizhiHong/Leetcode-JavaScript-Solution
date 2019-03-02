54bfe6a96b300873099bfdbb63ca27dc26419ad8
YizhiHong <yhong25@hawk.iit.edu>
2019-03-01 16:52:12 -0800

    update

---
+++ 739. Daily Temperatures.js
@@ -0,0 +1,25 @@
+/**
+Given a list of daily temperatures T, return a list such that, for each day in the input, tells you how many days you would have to wait until a warmer temperature. If there is no future day for which this is possible, put 0 instead.
+
+For example, given the list of temperatures T = [73, 74, 75, 71, 69, 72, 76, 73], your output should be [1, 1, 4, 2, 1, 1, 0, 0].
+
+Note: The length of temperatures will be in the range [1, 30000]. Each temperature will be an integer in the range [30, 100].
+
+
+*/
+
+//O(n) O(n)
+
+var dailyTemperatures = function(T) {
+    let stack = []
+    let res = new Uint16Array(T.length).fill(0)
+    
+    for(let i = 0; i < T.length; i++){
+        while(stack.length >=0 && T[i] > T[stack[stack.length-1]]){
+            let idx = stack.pop(i)
+            res[idx] = i - idx
+        }
+        stack.push(i)
+    }
+    return res
+};
