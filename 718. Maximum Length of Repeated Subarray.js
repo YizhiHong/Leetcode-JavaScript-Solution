8b7e594471e6d610e13dc35dbdeda7ffc39378ac
YizhiHong <yhong25@hawk.iit.edu>
2019-03-21 19:50:53 -0700

    update

--- 718. Maximum Length of Repeated Subarray.js
+++ 718. Maximum Length of Repeated Subarray.js
@@ -37,6 +37,71 @@ var findLength = function(A, B) {
 };
 
 /**
+O(n) space
+ */
+var findLength = function(A, B) {
+    let max = 0,
+        dp = new Array(B.length).fill(0)
+        
+    for(let i = 0; i< A.length; i++){
+        let prev = 0
+        for(let j = 0; j< B.length; j++){
+            let tmp = dp[j]
+            if(A[i] === B[j]){
+                if( (i=== 0 || j === 0) ) dp[j] = 1 
+                else dp[j] = prev + 1
+                if(max < dp[j]) max = dp[j]
+            }else{
+                dp[j] = 0
+            }
+            prev = tmp
+        }
+    }
+    return max
+};
+
+/**
+O(1) space: Diagonal travel
+ */
+
+ var findLength = function(A, B) {
+    let max = 0,
+        curr = 0,
+        prevIdx = 0
+
+    for(let offset = A.length - 1; offset >=0 ; offset--){
+        curr = 0
+        for(let i = 0 ; i < offset + 1 && prevIdx >= 0 ; i++){
+            prevIdx =  i + (A.length - 1 - offset)
+            if(A[i] === B[prevIdx]){
+                curr++
+                if(max < curr) max = curr
+            }else{
+                curr = 0
+            }
+        }
+    }
+    
+    for(let offset = B.length - 1; offset >=0 ; offset--){
+        curr = 0
+        for(let i = 0 ; i < offset + 1 && prevIdx >= 0 ; i++){
+            prevIdx =  i + (B.length - 1 - offset)
+            if(A[prevIdx] === B[i]){
+                curr++
+                if(max < curr) max = curr
+            }else{
+                curr = 0
+            }
+        }
+    }
+    
+    
+    return max
+};
+
+
+
+/**
     Binary search + hash Map: O( (N+M)*log(max(M,N)) ) O(M)
 */
 
