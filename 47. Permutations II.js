16b7a31ab57a46cc0a59fe11e37d917106bfc52a
YizhiHong <yhong25@hawk.iit.edu>
2019-04-07 12:41:23 -0700

    update permutation

---
+++ 47. Permutations II.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permuteUnique = function(nums) {
+    
+    let res = [],
+        used = new Array(nums.length).fill(false)
+    
+    nums.sort((a,b)=>a-b)
+    
+    permute([],res)
+    
+    function swap(arr,a,b){
+        let temp = arr[a]
+        arr[a] = arr[b]
+        arr[b] = temp
+    }
+    
+    function permute(arr,result){
+        if(arr.length === nums.length){
+            result.push([...arr])
+        }
+        for(let i = 0; i < nums.length; i++){
+            if (used[i]) continue
+            if( i>0 && nums[i] === nums[i-1] && used[i-1]) continue
+            used[i] = true
+            arr.push(nums[i])
+            permute(arr, result)
+            used[i] = false 
+            arr.pop()
+        }
+        
+    }
+    return 
+};
