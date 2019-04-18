f51859dec97875c5ae0e66fcfe6311b94375a962
YizhiHong <yhong25@hawk.iit.edu>
2019-04-17 23:13:08 -0700

    update

---
+++ 133. Clone Graph.js
@@ -0,0 +1,36 @@
+/**
+Given a reference of a node in a connected undirected graph, return a deep copy (clone) of the graph. Each node in the graph contains a val (int) and a list (List[Node]) of its neighbors.
+*/
+
+
+/**
+ * // Definition for a Node.
+ * function Node(val,neighbors) {
+ *    this.val = val;
+ *    this.neighbors = neighbors;
+ * };
+ */
+/**
+ * @param {Node} node
+ * @return {Node}
+ */
+var cloneGraph = function(node) {
+    let hashMap = new Map()
+    
+    return dfs(node,[], null)
+    
+    function dfs(target){
+        if (target === null ) return null
+        
+        if(hashMap.has(target.val)) return hashMap.get(target.val)
+        
+        let clone = new Node(target.val,[])
+        hashMap.set(clone.val,clone)
+        
+        for(let n of target.neighbors){
+            clone.neighbors.push(dfs(n))        
+        }
+
+        return clone
+    }
+};
