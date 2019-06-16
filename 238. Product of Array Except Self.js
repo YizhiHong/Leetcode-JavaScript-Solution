587d638375f926c766ba8272005654955136c5d8
YizhiHong <yhong25@hawk.iit.edu>
2019-06-16 00:33:45 -0400

    update 238

---
+++ 238. Product of Array Except Self.js
@@ -0,0 +1,34 @@
+/**
+ * 
+ * Given an array nums of n integers where n > 1,  return an array output such that output[i] is equal to the product of all the elements of nums except nums[i].
+
+Example:
+
+Input:  [1,2,3,4]
+Output: [24,12,8,6]
+Note: Please solve it without division and in O(n).
+
+Follow up:
+Could you solve it with constant space complexity? (The output array does not count as extra space for the purpose of space complexity analysis.)
+ * 
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+
+//O(n) O(1)
+var productExceptSelf = function(nums) {
+    const output = new Array(nums.length)
+    output[0] = 1
+    for(let i = 1 ; i< nums.length; i++){
+        output[i] = nums[i - 1]*output[i-1]
+    }
+    let right = 1
+    for (let i = nums.length -1; i>=0; i--){
+        output[i] = output[i] * right
+        right *= nums[i]
+    }
+    return output
+};
